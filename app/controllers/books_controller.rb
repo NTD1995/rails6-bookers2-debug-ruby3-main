3911e34e3bd5cbc77003ea0e9c191acdc6aba4f4
abedaiki <abe12261994@gmail.com>
2025-01-26 10:55:54 +0000

    コメント機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
       @user = @book.user
       @current_user  = current_user
+          @book_comment = BookComment.new
   end
 
   def index
