e4a4471d6308974d1117d974e9284919648aa4db
abedaiki <abe12261994@gmail.com>
2025-02-04 09:27:51 +0000

    自分がグループオーナーでない場合は、グループ詳細画面に「Join this group」ボタンを表示させる実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ApplicationRecord
-  has_many :group_users, dependent: :destroy
-  belongs_to :owner, class_name: "User"
-
-  validates :name, presence: true
+ validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :group_image
+   has_many :group_users, dependent: :destroy
+ has_many :users, through: :group_users, source: :user
+  belongs_to :owner, class_name: "User"
 
   def get_group_image
     (group_image.attached?) ? group_image : 'no_image.jpg'
@@ -12,5 +12,9 @@ class Group < ApplicationRecord
 
   def is_owned_by?(user)
     owner.id == user.id
+  end
+
+  def include_user?(user)
+    group_users.exists?(user_id: user.id)
   end
 end
