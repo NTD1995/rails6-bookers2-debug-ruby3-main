6d8e45d2ed67bd884c55806fa7887d9e5799ec83
abedaiki <abe12261994@gmail.com>
2025-02-04 07:10:24 +0000

    グループを作成の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'groups/new'
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/edit'
   get 'relationships/create'
   get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
