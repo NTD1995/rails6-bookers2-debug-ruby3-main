07fceca2a1237351149728120b2118ddee3d51d5
abedaiki <abe12261994@gmail.com>
2025-03-30 03:22:55 +0000

    グループオーナーのみメールの作成の修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   port:                 587,
+  #   address:              'smtp.gmail.com',
+  #   domain:               'gmail.com',
+  #   user_name: ENV['MAILER_ADDRESS'],
+  #   password: ENV['MAILER_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -75,15 +88,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            ENV['XXXXX'],
-    password:             ENV['XXXXX'], 
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
 end
